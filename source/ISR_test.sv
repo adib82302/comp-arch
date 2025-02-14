af44683e9d572b23eff7635f11b09101c1652177
adib82302 <adib82302@gmail.com>
2025-02-14 17:27:35 -0500

    project 2

---
+++ source/ISR_test.sv
@@ -0,0 +1,23 @@
+`timescale 1ns/1ps
+
+// P2 TODO: Write a testbench which tests both specific edge cases and random values.
+//          Base your testbench on mult_test.sv, specifically the wait_until_done task.
+
+module testbench;
+
+
+
+
+
+
+    initial begin
+
+        // if incorrect
+        $display("@@@ Incorrect");
+        // if correct
+        $display("@@@ Passed");
+
+        $finish;
+    end
+
+endmodule
